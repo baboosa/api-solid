21eb8b695d3d329c598d9cabd2deb6503c11b001
weverton <weverton.wrb10@gmail.com>
2025-07-30 22:01:00 -0300

    feat: add new routes and RBAC

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { verifyJWT } from '@/http/middleware/verify-jwt'
 import { nearby } from './nearby'
 import { create } from './create'
 import { search } from './search'
+import { verifyUserRole } from '@/http/middleware/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 
   app.get('/gyms/nearby', nearby)
   app.get('/gyms/search', search)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { verifyJWT } from '@/http/middleware/verify-jwt'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
