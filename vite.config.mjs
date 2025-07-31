83764a99273d2898df2745c2223f4d2e0b342faa
weverton <weverton.wrb10@gmail.com>
2025-07-30 22:01:48 -0300

    feat: migration and others

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: 'prisma',
+    environment: './prisma/vitest-environment-prisma',
     globals: true,
   },
 })
